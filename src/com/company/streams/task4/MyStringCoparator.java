3f32c4342bb7c379c2f3a294c19eb64eb85b2822
Vladislav Brinchuk <brinchuk.vlad@gmail.com>
2022-06-19 03:25:45 +0300

    added MyStringComparator.java

---
+++ src/com/company/streams/task4/MyStringCoparator.java
@@ -0,0 +1,12 @@
+package com.company.streams.task4;
+
+import java.util.Comparator;
+
+public class MyStringCoparator implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        return o1.compareToIgnoreCase(o2);
+    }
+    
+}
