aed3ffacc206fbf1b483cf2ddaa2be3eceb91c46
Vladislav Brinchuk <brinchuk.vlad@gmail.com>
2022-07-02 02:36:45 +0300

    modified, added hashmap

--- src/com/company/streams/task1/App.java
+++ src/com/company/streams/task1/App.java
@@ -1,19 +1,32 @@
 package com.company.streams.task1;
 
+import java.time.Month;
 import java.time.Year;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        solveTask(userInput());
+        PrintDaysOfMonthInYear(getDaysOfMonthInYear(userInput()));
     }
-    private static void solveTask(Integer userInputYear) {
-        Year year = Year.parse(userInputYear.toString());
-        int i = 1;
-        while (i <= 12) {
-            System.out.printf("Month %s in %d has %d days. %n", year.atMonth(i).getMonth(), userInputYear, year.atMonth(i++).lengthOfMonth());
+
+    private static void PrintDaysOfMonthInYear(HashMap<Integer, Integer> map) {
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.printf("%s has %d days. %n", Month.of(entry.getKey()), entry.getValue());
         }
     }
+
+    private static HashMap<Integer, Integer> getDaysOfMonthInYear(Integer userInputYear) {
+        Year year = Year.parse(userInputYear.toString());
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int i = 1;
+        while (i <= 12) {
+            map.put(i, year.atMonth(i++).lengthOfMonth());
+        }
+        return map;
+    }
+
     private static Integer userInput() {
         System.out.print("Input year: ");
         Scanner scanner = new Scanner(System.in);
@@ -21,10 +34,10 @@ public class App {
         Integer userInput = null;
         try {
             userInput = Integer.parseInt(str);
-        }catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {
             throw new NumberFormatException("Incorrect input. ");
         }
-        if(userInput < 1) {
+        if (userInput < 1) {
             System.out.println("Incorrect value. We set default value as 2022.");
             userInput = 2022;
         }
