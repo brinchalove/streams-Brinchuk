dbff562ca955fdbe08f232fad5b458e9a7cb21a0
Vladislav Brinchuk <brinchuk.vlad@gmail.com>
2022-07-02 06:02:13 +0300

    modified task3

--- src/com/company/streams/task3/App.java
+++ src/com/company/streams/task3/App.java
@@ -3,32 +3,39 @@ package com.company.streams.task3;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        solveTask(userInput());
+        printMonthsWithFridays13h(getMonthsWithFridays13h(getUserInput()));
     }
 
-    private static void solveTask(Integer year) {
+    private static void printMonthsWithFridays13h(List<Integer> list) {
+        list.forEach(m -> System.out.println(Month.of(m) + " has Friday 13th."));
+    }
+
+    private static List<Integer> getMonthsWithFridays13h(Integer year) {
         int i = 1;
         boolean isExist = false;
-        while (i <= 12) {
+        List<Integer> list = new ArrayList<>();
+        while (i <= Month.values().length) {
             LocalDate date = LocalDate.of(year, i, 13);
-            if(date.getDayOfWeek() == DayOfWeek.FRIDAY) {
+            if (date.getDayOfWeek() == DayOfWeek.FRIDAY) {
                 isExist = true;
-                System.out.println(Month.of(i) + " in " + year + " has Friday 13th.");
+                list.add(i);
             }
             i++;
         }
-        if(!isExist) {
+        if (!isExist) {
             System.out.printf("Year %d no have fridays 13th. ", year);
         }
-        
+        return list;
     }
 
-    private static Integer userInput() {
+    private static Integer getUserInput() {
         System.out.print("Input year: ");
         int year;
         try {
@@ -38,7 +45,7 @@ public class App {
             System.out.println("Incorrect value. We set default value as 2022.");
             year = 2022;
         }
-        if(year < 1) {
+        if (year < 1) {
             System.out.println("Incorrect value. We set default value as 2022.");
             year = 2022;
         }
